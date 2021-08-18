225a8bde1bccf222fae24a2335c6bc990d2e7d21
hiddencilantro <stevekim1109@gmail.com>
2021-08-18 12:07:09 -0400

    commit tree e73d366804324d0c324ea6bf350e0e75683a5269

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter(s => s.toLowerCase() === string.toLowerCase());
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(s => s.toLowerCase().indexOf(string.toLowerCase()) === 0);
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(o => o.name === string);
+}
